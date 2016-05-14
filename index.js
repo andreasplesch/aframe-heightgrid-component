9d389327331ed89c4cac2fa91292dc50c311f738
Andreas Plesch <andreasplesch@netscape.net>
2016-05-14 11:41:29 -0400

    vts

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ AFRAME.registerComponent('heightgrid', {
 			default: 1 },
 		heights: {
 			default: [],
-			parse: function (value) { return parseFloats (value) } ,
-			stringify: function (value) { return stringifyFloats (value) }
+			parse: function (value) { return parseNumbers (value) } ,
+			//stringify: function (value) { return value.toString() }
 		}
 	}, 
 
@@ -34,7 +34,29 @@ AFRAME.registerComponent('heightgrid', {
    * Called when component is attached and when component data changes.
    * Generally modifies the entity based on the data.
    */
-  update: function (oldData) { },
+  update: function (oldData) {
+    var el = this.el;
+    var data = this.data;
+    var xdimension = data.xdimension;
+    var zdimension = data.zdimension;
+    var xspacing = data.xspacing;
+    var zspacing = data.zspacing;
+    
+    var h = data.heights;
+    var h = h.length ? h:[0];
+    
+    //vertices
+    var vts = [];
+    for (var z = 0; z < zdimension; z++) {
+      for (var x = 0; x < xdimension; x++) {
+        vts.push( new THREE.Vector3 (
+          x * xspacing, h[(x + z * xdimension)%h.length)], z * zspacing ) ); 
+      }
+    }
+    
+    el.setAttribute('faceset', { triangles: faces, vertices: vts } );
+    
+  },
 
   /**
    * Called when a component is removed (e.g., via removeAttribute).
@@ -59,3 +81,16 @@ AFRAME.registerComponent('heightgrid', {
    */
   play: function () { },
 });
+
+function parseNumbers (value) {
+	if (typeof value === 'object') {return value}
+	var mc = value.match(/([+\-0-9eE\.]+)/g);
+	var numbers = [];
+	//var vec = {};
+	for (var i=0, n=mc?mc.length:0; i<n; i++) {
+		//vec = new THREE.Vector2(+mc[i+0], +mc[i+1]);
+		numbers.push( parseFloat( mc[i], 10 ) );
+	}
+	return numbers;
+}
+
